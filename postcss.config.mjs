58ada9e3cf7e0830fde03b1639000998cb551c9d
riqujavier <riquelmejavierariel@gmail.com>
2025-02-01 19:14:46 -0300

    Refactor Tailwind and PostCSS configuration; enhance Hero component styles and animations

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,5 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-const config = {
-    plugins: [
-        tailwindcss,
-        autoprefixer,
-    ],
-};
-
-export default config;
+export default {
+    plugins: {
+      "@tailwindcss/postcss": {},
+    }
+  }
