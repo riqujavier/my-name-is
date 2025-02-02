45e94b92bb24de40525595b9a1881c8313bd08a9
riqujavier <riquelmejavierariel@gmail.com>
2025-02-02 13:11:43 -0300

    Refactor Footer component styles for improved layout and responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-gray-800 text-white py-4">
-            <div className="container mx-auto text-center">
+        <footer className=" text-white py-4">
+            <div className="container max-w-4xl mx-auto px-4 md:px-14 py-8 text-left">
                 <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
-                <div className="mt-2">
+                <div className="mt-2 ">
                     <a href="#" className="text-gray-400 hover:text-white mx-2">Privacy Policy</a>
                     <a href="#" className="text-gray-400 hover:text-white mx-2">Terms of Service</a>
                     <a href="#" className="text-gray-400 hover:text-white mx-2">Contact Us</a>
